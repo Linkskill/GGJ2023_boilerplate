cb07e3ce7adbfaf15dac97575488284709d0d79b
Lincoln Batista <linkskill@gmail.com>
2023-02-01 22:22:13 -0300

    multitextures scenario added

--- nodejs/public/index.js
+++ nodejs/public/index.js
@@ -15,7 +15,7 @@ const scene = new THREE.Scene();
 console.log(scene);
 
 // Camera Setup
-const fov = 45;
+const fov = 50;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1;
 const far = 1000;
@@ -40,23 +40,34 @@ console.log(renderer);
 // Adding orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-// making sphere
-const geometry = new THREE.SphereGeometry(5, 10, 10);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xE6345E,
-    wireframe: true
-});
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+controls.minDistance = 10;
+controls.maxDistance = 40;
 
-// making inner sphere
-const innerGeometry = new THREE.SphereGeometry(3, 20, 20);
-const innerMaterial = new THREE.MeshBasicMaterial({
-    color: 0xE6DD67,
-    wireframe: true
-});
-const innerSphere = new THREE.Mesh(innerGeometry, innerMaterial);
-scene.add(innerSphere);
+let loader = new THREE.TextureLoader();
+
+let textureArray = [];
+
+let frontTexture = loader.load('./models/MarriottMadisonWest/front.jpg')
+let backTexture = loader.load('./models/MarriottMadisonWest/back.jpg')
+let topTexture = loader.load('./models/MarriottMadisonWest/top.jpg')
+let bottomTexture = loader.load('./models/MarriottMadisonWest/bottom.jpg')
+let leftTexture = loader.load('./models/MarriottMadisonWest/left.jpg')
+let rightTexture = loader.load('./models/MarriottMadisonWest/right.jpg')
+
+textureArray.push(new THREE.MeshBasicMaterial({ map: frontTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: backTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: topTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: bottomTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: leftTexture }));
+textureArray.push(new THREE.MeshBasicMaterial({ map: rightTexture }));
+
+for(let i = 0; i < textureArray.length; i++) {
+    textureArray[i].side = THREE.BackSide;
+}
+
+const cubeGeometry = new THREE.BoxGeometry(100, 100, 100);
+const skyBox = new THREE.Mesh(cubeGeometry, textureArray);
+scene.add(skyBox);
 
 // render function to render the scene
 const render = () => {
@@ -66,8 +77,6 @@ const render = () => {
 // Recursion function for animation
 const animate = () => {
     requestAnimationFrame(animate);
-    sphere.rotation.y += 0.005;
-    innerSphere.rotation.y -= 0.01
     render();
     stats.update();
 }
